1b3deca63581cf59c1bc3e980256fd352a1bf963
MartynasDruteika <martynasdd@gmail.com>
2023-01-23 20:12:32 +0200

    Show careers list in /careers page

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const path = require('path');
 const express = require('express');
+const mysql = require('mysql2');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+const mysqlConfig = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'test123',
+    database: 'ssr_pamoka',
+    port: 3306
+}
+
+const connection = mysql.createConnection(mysqlConfig);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,6 +27,16 @@ app.get('/', (req, res) => {
 app.get('/contacts', (req, res) => {
     // res.sendFile(path.join(__dirname, 'views', 'contacts.html'));
     res.render('contacts', { text: 'This is contacts page content passed dynamically', pageTitle: 'Contacts page' });
+});
+
+app.get('/careers', (req, res) => {
+    connection.execute('SELECT * FROM careers', (err, careers) => {
+        if (err) {
+            res.sendStatus(500);
+        } else {
+            res.render('careers', { pageTitle: 'Careers page', careers });
+        }
+    })
 });
 
 app.get('/api/restaurants', (req, res) => {
